b1783b2e3c3c470249032877cf67ec589197aa30
Shima <shima.jin@icloud.com>
2018-11-02 19:17:38 +0900

    Add Proactive Suggestion

--- autofill-practice/ViewController.swift
+++ autofill-practice/ViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let emailTextField = UITextField()
+    let pinCodeTextField = UITextField()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        // Proactive Suggestions
+        // emailなどtextContentTypeに指定したコンテンツを入力補完として表示
+        // UITextInputTraitsに準拠しているUITextViewとUITextFieldで使用できる
+        // 他のアプリで最近入力したデータが候補として表示される
+        
+        emailTextField.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width - 40, height: 40)
+        emailTextField.center = CGPoint(x: view.center.x, y: view.center.y - 100)
+        emailTextField.textContentType = .emailAddress
+        emailTextField.keyboardType = .emailAddress
+        emailTextField.backgroundColor = .groupTableViewBackground
+        view.addSubview(emailTextField)
+        
+        pinCodeTextField.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width - 40, height: 40)
+        pinCodeTextField.center = view.center
+        if #available(iOS 12.0, *) {
+            pinCodeTextField.textContentType = .oneTimeCode
+        }
+        pinCodeTextField.keyboardType = .numbersAndPunctuation
+        pinCodeTextField.backgroundColor = .groupTableViewBackground
+        view.addSubview(pinCodeTextField)
 
+        
+        
+    }
 
 }
 
